d3eec9c2aabfd9883f01ff8e591972f1994a02e4
vlad <mister-frize@hotmail.fr>
2024-12-02 19:11:13 +0100

    Update Spotify.Target.cs
    UE 5.4

--- Source/Spotify.Target.cs
+++ Source/Spotify.Target.cs
@@ -8,7 +8,7 @@ public class SpotifyTarget : TargetRules
 	public SpotifyTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "Spotify" } );
 	}
